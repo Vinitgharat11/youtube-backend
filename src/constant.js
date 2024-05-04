8f8ac0072dc9bf2e73df24e5fd197bfc12ddee62
vint <vinitgharat99@gmail.com>
2024-05-04 20:49:17 +0530

    utils config

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,3 @@
 export const DB_NAME = 'youtube'
+import express from 'express'
+export const app = express()
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import {DB_NAME} from '../constant.js'
-const connectDB = ()=>{
+const connectDB = async ()=>{
   try {
-    mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("MongoDb Connection Failed ",error)
     process.exit(1)
