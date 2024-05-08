81abf731ac587f8ba85bc7943806fd04a3e12e68
vint <vinitgharat99@gmail.com>
2024-05-08 20:51:00 +0530

    production setup done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
+
+const uploadOnCloudinary = async (localfilepath) => {
+  try {
+    if (!localfilepath) return null;
+    const response = await cloudinary.uploader.upload(localfilepath, {
+      resource_type: "auto",
+    });
+    console.log("file upload on cloudinary ",response.url)
+  } catch (error) {
+    fs.unlinkSync(localfilepath);
+    return null;
+  }
+};
