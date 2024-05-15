a09d0cea7b9a3f4dd8c1e60f4e913eb0926506e3
vint <vinitgharat99@gmail.com>
2024-05-15 20:31:58 +0530

    Fix : Image store in local when uplade in clodinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,9 +17,10 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     // file has been uploaded successfull
     console.log("file is uploaded on cloudinary ", response.url);
+
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     console.log("clodinary file not uploaded");
     return null;
   }
